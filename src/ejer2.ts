f18133c17ac365094ce44c6b2bffc610bd486443
eric <eric.durr.20@ull.edu.es>
2021-05-02 00:59:15 +0100

    finished exercise 23

--- src/ejer2.ts
+++ src/ejer2.ts
@@ -1,6 +1,4 @@
 import * as fs  from 'fs';
- import * as yrgs from "yargs";
-
 if(process.argv.length < 4) {
   console.error("Less arguments than expected");
   console.error(`Usage: node dist/ejer2 <filename> <action/s>`);
@@ -14,25 +12,44 @@ if(process.argv.length < 4) {
     if (err) {
       console.error(`File ${filename} does not exist`);
     } else {
-      yrgs.command({
-        command: 'lines',
-        describe: 'Count the lines in a file',
-        handler(argv) {
+      for (let i = 3; i < process.argv.length; i++) {
+        // console.log(process.argv[i]);
+        if (process.argv[i].toString() === "--lines") {
+          const rs = fs.createReadStream(filename); 
+          let i: number = 0
+          let nLines: number = 0
+          rs.on('data', function(chunk) {
+            for (i=0; i < chunk.length; ++i)
+              if (chunk[i] == 10) nLines++;
+          })
+          .on('end', function() {
+            console.log(`File ${filename} has ${nLines + 1} lines.`);
+          }); 
         
+        } else if (process.argv[i].toString() === "--words") {
+          const rs = fs.createReadStream(filename); 
+          let nWords: number = 0
+          rs.on('data', function(chunk) {
+            nWords += chunk.toString().split(" ").length;
+          })
+          .on('end', function() {
+            console.log(`File ${filename} has ${nWords} words.`);
+          }); 
+        } else if (process.argv[i].toString() === "--chars") {
+          const rs = fs.createReadStream(filename); 
+          let nChars: number = 0
+          rs.on('data', function(chunk) {
+            nChars += chunk.toString().split("").length;
+          })
+          .on('end', function() {
+            console.log(`File ${filename} has ${nChars} characters.`);
+          }); 
+        } else {
+          console.log ("option " + 
+                        process.argv[i].toString() +
+                        " not available");
         }
-       })  
-       const rs = fs.createReadStream(filename);
-          
-       let i: number = 0
-       let nLines: number = 0
-       rs.on('data', function(chunk) {
-         for (i=0; i < chunk.length; ++i)
-           if (chunk[i] == 10) nLines++;
-       })
-       .on('end', function() {
-         console.log(nLines);
-       }); 
+      }
     }
   });
-  console.log("Accepted input");
 }
